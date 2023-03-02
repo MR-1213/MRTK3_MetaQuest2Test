2885739dc6e26d19b72de0f3a109e46e5e69de5f
M.R <m.r.MeijoUniv@gmail.com>
2023-03-02 22:42:12 +0900

    [add] PLATEAU SDKを導入

--- MRTK3_MetaQuest2/Assets/Scripts/PlayerRespawnController.cs
+++ MRTK3_MetaQuest2/Assets/Scripts/PlayerRespawnController.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerRespawnController : MonoBehaviour
 {
     [SerializeField, Tooltip("The distance from the GameObject's spawn position at which will trigger a respawn.")]
-    private float distanceThreshold = 20.0f;
+    private float distanceThreshold = 30.0f;
 
     private Vector3 localRespawnPosition;
     private Quaternion localRespawnRotation;
     private Rigidbody rigidBody;
     private float distanceThresholdSquared;
+
+    private void Awake() 
+    {
+        if(distanceThreshold < 0)
+        {
+            Debug.LogError("distanceThresholdの値は正の値を指定してください");
+        }
+    }
 
     private void Start()
     {
@@ -22,9 +30,12 @@ public class PlayerRespawnController : MonoBehaviour
 
     private void LateUpdate()
     {
+        //カッコ内の値はマイナスの値になる可能性があるので2乗してその値で考える
         float distanceSqr = (localRespawnPosition - transform.localPosition).sqrMagnitude;
+        //現在のY座標を取得
+        float currentPositionY = transform.localPosition.y;
 
-        if (distanceSqr > distanceThresholdSquared)
+        if (distanceSqr > distanceThresholdSquared && currentPositionY < -3)
         {
             // Reset any velocity from falling or moving when respawning to original location
             if (rigidBody != null)
