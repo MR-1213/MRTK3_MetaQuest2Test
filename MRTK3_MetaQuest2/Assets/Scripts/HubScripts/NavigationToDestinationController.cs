e17c5a918d83fd66b5bc206696215626324eae05
M.R <m.r.MeijoUniv@gmail.com>
2023-03-10 02:40:26 +0900

    [add] ゲーム開始のUI周りのスクリプト追加

---
+++ MRTK3_MetaQuest2/Assets/Scripts/HubScripts/NavigationToDestinationController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationToDestinationController : MonoBehaviour
+{
+    public Transform destination_GFO;
+
+    [SerializeField] private GameObject targetBuilding;
+    [SerializeField] private Material hightlightMaterial;
+    [SerializeField] private Transform cursor;
+
+    private NavMeshAgent navMeshAgent;
+    private float distanceThreshold = 5.0f;
+
+    private void Start() 
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(destination_GFO.position);
+        navMeshAgent.updatePosition = false;
+        navMeshAgent.updateRotation = false;    
+    }
+
+    public void StartNavigation()
+    {
+        cursor.gameObject.SetActive(true);
+        targetBuilding.GetComponent<Renderer>().material = hightlightMaterial;
+        StartCoroutine(NavigationToDestination());
+    }
+
+    IEnumerator NavigationToDestination()
+    {
+        while(true)
+        {
+            float distance = (this.transform.position - destination_GFO.position).sqrMagnitude;
+            if(distance < distanceThreshold * distanceThreshold)
+            {
+                cursor.gameObject.SetActive(false);
+                yield break;
+            }
+
+            cursor.rotation = Quaternion.LookRotation(navMeshAgent.steeringTarget - transform.position, Vector3.up);
+            navMeshAgent.nextPosition = transform.position;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
